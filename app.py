8afce3a4d4a45f270802123bf339acf1da1d6d89
Karen <notkarenyi@gmail.com>
2025-03-27 00:32:32 -0500

    working graph

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 from shiny import reactive
 from shinywidgets import render_plotly
+import igraph as ig
+import numpy as np
 
 df = pd.read_excel('resources/resume.xlsx',sheet_name='Graph',parse_dates=['Start','End'])
 
@@ -14,6 +16,24 @@ df['Interests'] = df['Interests'].fillna('N/A')
 
 df = df.sort_values('Start')
 df['Dummy'] = 1
+
+df['Title'] = df['Course/Role'] + df['Organization']
+df['Connection'] = [x.split(', ') if isinstance(x,str) else [] for x in df['Connection']]
+df = df.explode('Connection')
+graph = df[['Title','ID','Connection']]
+graph['ID'] = graph['ID'].astype('int64')
+graph = graph.rename(columns={
+    'Title':'label',
+    'ID':'from',
+    'Connection':'to'
+})
+edges = graph.loc[~graph['to'].isnull(),['from','to']]
+edges['to'] = edges['to'].astype('int64')
+G = ig.Graph.DataFrame(
+    edges, 
+    directed=True,
+    # vertices=graph[['from','label']]
+)
 
 # print(df['StartMonth'])
 # print(df.loc[df['StartYear']==2023])
@@ -53,6 +73,91 @@ ui.input_selectize(
 @render.code
 def click_info():
     return str(click_reactive.get())
+
+@render_plotly  
+def graph():  
+    # labels=list(G.vs['label'])
+    # N=len(labels)
+    N = len(G.vs)
+    E=[e.tuple for e in G.es]# list of edges
+    layt=G.layout('kk') #kamada-kawai layout
+        
+    Xn=[layt[k][0] for k in range(N)]
+    Yn=[layt[k][1] for k in range(N)]
+    Xe=[]
+    Ye=[]
+    for e in E:
+        Xe+=[layt[e[0]][0],layt[e[1]][0], None]
+        Ye+=[layt[e[0]][1],layt[e[1]][1], None]
+
+    trace1=go.Scatter(x=Xe,
+        y=Ye,
+        mode='lines',
+        line= dict(color='rgb(210,210,210)', width=1),
+        hoverinfo='none'
+    )
+    trace2=go.Scatter(x=Xn,
+        y=Yn,
+        mode='markers',
+        name='ntw',
+        marker=dict(symbol='circle-dot',
+                                    size=5,
+                                    color='#6959CD',
+                                    line=dict(color='rgb(50,50,50)', width=0.5)
+                                    ),
+        # text=labels,
+        # hoverinfo='text'
+    )
+
+    axis=dict(
+        showline=False, # hide axis line, grid, ticklabels and  title
+        zeroline=False,
+        showgrid=False,
+        showticklabels=False,
+        title=''
+    )
+
+    width=800
+    height=800
+    # layout=go.Layout(title= "Coauthorship network of scientists working on network theory and experiment"+\
+    #             "<br> Data source: <a href='https://networkdata.ics.uci.edu/data.php?id=11'> [1]</a>",
+    #     font= dict(size=12),
+    #     showlegend=False,
+    #     autosize=False,
+    #     width=width,
+    #     height=height,
+    #     xaxis=layout.XAxis(axis),
+    #     yaxis=layout.YAxis(axis),
+    #     margin=layout.Margin(
+    #         l=40,
+    #         r=40,
+    #         b=85,
+    #         t=100,
+    #     ),
+    #     hovermode='closest',
+    #     annotations=[
+    #         dict(
+    #         showarrow=False,
+    #             text='This igraph.Graph has the Kamada-Kawai layout',
+    #             xref='paper',
+    #             yref='paper',
+    #             x=0,
+    #             y=-0.1,
+    #             xanchor='left',
+    #             yanchor='bottom',
+    #             font=dict(
+    #             size=14
+    #             )
+    #             )
+    #         ]
+    #     )
+
+    data=[trace1, trace2]
+    fig=go.Figure(
+        data=data, 
+        # layout=layout
+    )
+    return fig
 
 # Create Gantt chart
 # The native function does not work with Shiny for some reason
